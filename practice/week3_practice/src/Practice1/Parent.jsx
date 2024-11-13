270e2d8e41b55cc7920ca4e9a2e2742ead538bda
imddoy <imddoy@duksung.ac.kr>
2024-11-13 23:49:24 +0900

    3주차 실습

--- practice/week3_practice/src/Practice1/Parent.jsx
+++ practice/week3_practice/src/Practice1/Parent.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Child from './Child';
 
 function Parent(){
   const [parentAge, setParentAge] = useState(0);
-  const [childAge, setChildAge] = useState(0);
 
   const incrementParentAge = () => {
     setParentAge(parentAge + 1);
   };
 
-  const incrementChildAge = () => {
-    setChildAge(childAge + 1);
-  };
-
   console.log('👪부모 컴포넌트가 렌더링 되었습니다.');
+
+  const tellMe = useCallback(() => {
+    console.log('리심스 화이팅👍');
+  }, []); //useCallback을 사용하여 함수를 memoization한다.
 
   return(
     <div style = {{ border: '2px solid navy', padding: '10px'}}>
       <h1>👪부모</h1>
       <p>age: {parentAge}</p>
       <button onClick={incrementParentAge}>부모 나이 증가</button>
-      <button onClick={incrementChildAge}>자식 나이 증가</button>
-      <Child name={'홍길동'} age={childAge} />
+      <Child name={"김태욱"} tellMe={tellMe}/>
     </div>
   );
-}
-export default Parent
+};
+
+export default Parent;
